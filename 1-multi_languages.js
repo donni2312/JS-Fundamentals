edae8fda4bc5f731b1077a928aa70b0e7bcf2b5c
donni2312 <a.donnelly2312@gmail.com>
2025-05-20 23:03:31 +0200

    add file 1-multi_languages.js

---
+++ 1-multi_languages.js
@@ -0,0 +1,20 @@
+/*Write a script that prints 3 lines:
+
+The first line: “C is fun”
+The second line: “Python is cool”
+The third line: “JavaScript is amazing”
+You must use console.log(...) to print all output
+You are not allowed to use var
+guillaume@ubuntu:~/0x12$ node 1-multi_languages.js 
+C is fun
+Python is cool
+JavaScript is amazing
+guillaume@ubuntu:~/0x12$ 
+*/
+
+let myVar = "C is fun";
+console.log(myVar);
+myVar = "Python is cool";
+console.log(myVar);
+myVar = "JavaScript is amazing";
+console.log(myVar);
